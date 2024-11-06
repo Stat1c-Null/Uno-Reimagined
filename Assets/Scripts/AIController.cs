ae34d8c5f867c4c1da9199eeb566b5b3e8227c3d
Mikita Slabysh <slabysh2015@gmail.com>
2024-11-05 20:00:33 -0500

    Simple card choice for AI

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -7,6 +7,9 @@ public class AIController : MonoBehaviour
 
     public GameObject GameManager;
     private List<Texture2D> aiHand = new List<Texture2D>();
+    private string lastPlacedCard;
+    private string lastPlacedColor;
+    private int lastPlacedNumber;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,22 @@ public class AIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameManager.GetComponent<CardSelector>().isAiTurn) {
+            lastPlacedCard = GameManager.GetComponent<CardSelector>().lastPlacedCard;
+            string[] extract = lastPlacedCard.Split('_');
+            lastPlacedColor = extract[0];
+            lastPlacedNumber = int.Parse(extract[1]);
+            foreach(Texture2D card in aiHand) {
+                string[] extractCard = card.name.Split('_');
+                string cardColor = extractCard[0];
+                int cardNumber = int.Parse(extractCard[1]);
+                if(cardColor == lastPlacedColor && cardNumber > lastPlacedNumber) {
+                    Debug.Log(card.name);
+                    GameManager.GetComponent<CardSelector>().isAiTurn = false;
+                } else { 
+                    Debug.Log("I dont have a card");
+                }
+            }
+        }
     }
 }
